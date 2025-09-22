2ec99900438ba8bf024740c85389f435a4c88894
MDALAMRAN11 <44mdalamran44@gmail.com>
2025-09-23 02:10:07 +0600

    updated again

--- src/Components/SelectedPlayers/SelectedPlayers.jsx
+++ src/Components/SelectedPlayers/SelectedPlayers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SelectedPlayer from "./SelectedPlayer";
 
 const SelectedPlayers = ({
-    handleAddMorePlayers = { handleAddMorePlayers },
+    handleAddMorePlayers,
     selectedPlayer: players,
     deleteHandleSelectedPlayer,
 }) => {
